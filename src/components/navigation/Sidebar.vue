81e71519534f2de3ce16eef06c717ad3ba24f046
Godfred Nai <godfrednai@gmail.com>
2024-03-25 22:43:43 +0000

    created navigation components

---
+++ src/components/navigation/Sidebar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="py-4 w-full">
+    <div>
+      <p class="text-center text-white font-bold text-2xl mb-6">Logo</p>
+    </div>
+    <nav class="grid grid-cols-1 gap-4 text-white">
+      <RouterLink to="/dashboard">
+        <div
+          class="flex flex-col gap-2 w-full justify-center items-center hover:bg-[--secondary-blue] py-2">
+          <div class="p-2 w-10 h-10 rounded bg-zinc-100"></div>
+          <span class="text-sm leading-none">Dashboard</span>
+        </div>
+      </RouterLink>
+
+      <RouterLink to="/products">
+        <div
+          class="flex flex-col gap-2 w-full justify-center items-center hover:bg-[--secondary-blue] py-2">
+          <div class="p-2 w-10 h-10 rounded bg-zinc-100"></div>
+          <p class="text-sm leading-none">Products</p>
+        </div>
+      </RouterLink>
+
+      <RouterLink to="/sell">
+        <div
+          class="flex flex-col gap-2 w-full justify-center items-center hover:bg-[--secondary-blue] py-2">
+          <div class="p-2 w-10 h-10 rounded bg-zinc-100"></div>
+          <p class="text-sm leading-none">Sell</p>
+        </div>
+      </RouterLink>
+
+      <RouterLink to="/transactions">
+        <div
+          class="flex flex-col gap-2 w-full justify-center items-center hover:bg-[--secondary-blue] py-2">
+          <div class="p-2 w-10 h-10 rounded bg-zinc-100"></div>
+          <p class="text-sm leading-none">Transactions</p>
+        </div>
+      </RouterLink>
+
+      <RouterLink to="/analytics">
+        <div
+          class="flex flex-col gap-2 w-full justify-center items-center hover:bg-[--secondary-blue] py-2">
+          <div class="p-2 w-10 h-10 rounded bg-zinc-100"></div>
+          <p class="text-sm leading-none">Analytics</p>
+        </div>
+      </RouterLink>
+    </nav>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/navigation/Topbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="py-3 px-12 sticky top-0 bg-[--background-color] shadow">
+    <div class="flex justify-between items-center">
+      <div class="">
+        <p class="text-xl font-semibold">Welcome, <span class="text-gray-800 text-2xl font-bold">User</span></p>
+      </div>
+      <div class="flex items-center gap-4">
+        <RouterLink to="/login"><p class="text-red-600">Notifications</p></RouterLink>
+        <RouterLink to="/login"><p class="text-red-600">Logout</p></RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
