3919b7159b7577b0c431260abd8951cec8b8b58d
Godfred Nai <godfrednai@gmail.com>
2024-03-25 22:41:51 +0000

    created routes for views

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,46 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: () => import("../views/Dashboard.vue"),
+      children: [
+        {
+          path: "/login",
+          name: "login",
+          component: () => import("../views/Login.vue"),
+        },
+        {
+          path: "/analytics",
+          name: "analytics",
+          component: () => import("../views/Analytics.vue"),
+        },
+        {
+          path: "/transactions",
+          name: "transactions",
+          component: () => import("../views/Transactions.vue"),
+        },
+        {
+          path: "/dashboard",
+          name: "dashboard",
+          component: () => import("../views/Home.vue"),
+        },
+        {
+          path: "/sell",
+          name: "sell",
+          component: () => import("../views/Sell.vue"),
+        },
+        {
+          path: "/products",
+          name: "products",
+          component: () => import("../views/Products.vue"),
+        },
+      ],
     },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
