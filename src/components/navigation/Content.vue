8bb60314dcb639fa25a5a530d15c340b901a5db4
iamkingsley <callmeking@gmail.com>
2024-04-01 01:34:36 +0000

    minor changes

---
+++ src/components/navigation/Content.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Hello Its Me kingsley</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ab odio delectus harum quas neque iste reiciendis modi dolorum officia Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta blanditiis beatae facere temporibus alias, deleniti eos quas dolor ducimus tempore.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
