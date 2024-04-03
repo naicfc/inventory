df47eeca9837768682bc9d785bcaf3975d5ce967
Godfred Nai <godfrednai@gmail.com>
2024-04-03 02:56:08 +0000

    added navigator component

---
+++ src/components/navigation/Navigator.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex gap-2 items-center justify-between px-12 py-4">
+    <div class="flex gap-2">
+      <button
+        class="px-2 border-2 border-gray-300 text-xl rounded"
+        @click="back">
+        &lt
+      </button>
+      <button
+        class="px-2 border-2 border-gray-300 text-xl rounded"
+        @click="foward">
+        &gt
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    foward() {
+      this.$router.go(1);
+    },
+    back() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
