325bce0b74186d7a3f0eb09b4824bb8ff929f1ab
Godfred Nai <godfrednai@gmail.com>
2024-04-09 23:54:49 +0000

    minor changes

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -5,14 +5,22 @@
         <h1 class="header">Cart</h1>
       </div>
     </div>
+    <div>
+      <div v-for="item in cart" :key="item._id">
+        <div>
+          {{ item }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { useCartStore } from "@/stores/cart";
-import { onBeforeMount } from "vue";
+import { onBeforeMount, ref } from "vue";
 
 const cartStore = useCartStore();
+const cart = ref(cartStore.cart);
 
 onBeforeMount(() => {
   console.log(cartStore.cart);
